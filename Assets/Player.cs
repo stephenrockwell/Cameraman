45b4c577931c81844886da9f5cdc40fd60907877
dsjohnson <34867686+dsmarsh7@users.noreply.github.com>
2020-04-01 15:02:35 -0400

    The files

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SnapshotCamera snapCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            snapCam.CallTakeSnapshot();
+        }
+    }
+}
